dc6dc042e31e280ea944410e5a77d67ef6c00a19
martinb31753 <martinbotta85@gmail.com>
2023-10-09 20:27:56 -0300

    modificando puerto

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.get('/login', (req, res) => {
     res.sendFile(__dirname + '/views/login.html');
 });
 
-const port = 3030;
+const port = process.env.PORT || 3030;
 app.listen(port, () => {
     console.log(`Servidor iniciado en http://localhost:${port}`);
 });
